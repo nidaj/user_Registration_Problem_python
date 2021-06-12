7fee1064a07c5993a8f64f6404a68c9332becb05
nidaj <nida.jawre@gmail.com>
2021-06-12 15:19:02 +0530

    Merged: user registration with all validation

---
+++ user_registration_main.py
@@ -0,0 +1,23 @@
+'''
+Author: Nida Jawre
+Date of implementation: 12th june 2021
+
+'''
+
+
+
+
+from user_registration import UserRegistration
+
+if __name__ == '__main__':
+    user_reg = UserRegistration()
+    first_name_input = input("Enter First Name:")
+    user_reg.first_name_validation(first_name_input)
+    last_name_input = input("Enter Last Name:")
+    user_reg.last_name_validation(last_name_input)
+    email_input = input("Enter Email:")
+    user_reg.email_validation(email_input)
+    phone_input = input("Enter Phone:")
+    user_reg.phone_number_validation(phone_input)
+    password_input = input("Enter Password:")
+    user_reg.password_validation(password_input)
