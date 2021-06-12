fd23fb38439f9eb51ed51aeaf4fd5533d4b7b1aa
nidaj <nida.jawre@gmail.com>
2021-06-12 15:00:45 +0530

    Add: phone number validation

---
+++ user_registration_main.py
@@ -0,0 +1,12 @@
+from user_registration import UserRegistration
+
+if __name__ == '__main__':
+    user_reg = UserRegistration()
+    first_name_input = input("Enter First Name:")
+    user_reg.first_name_validation(first_name_input)
+    last_name_input = input("Enter Last Name:")
+    user_reg.last_name_validation(last_name_input)
+    email_input = input("Enter Email:")
+    user_reg.email_validation(email_input)
+    phone_input = input("Enter Phone:")
+    user_reg.phone_number_validation(phone_input)
