42899c91c62545a17d18ae625672b1c235cbaa03
nidaj <nida.jawre@gmail.com>
2021-06-12 15:11:36 +0530

    Delete UC1_First_Name_Validation directory

--- UC1_First_Name_Validation/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from user_registration import UserRegistration
-
-
-@pytest.fixture
-def user_register():
-    return UserRegistration()
--- UC1_First_Name_Validation/user_registration_main.py
+++
@@ -1,6 +0,0 @@
-from user_registration import UserRegistration
-
-if __name__ == '__main__':
-    user_reg = UserRegistration()
-    first_name_input = input("Enter First Name:")
-    user_reg.first_name_validation(first_name_input)
