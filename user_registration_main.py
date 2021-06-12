3e7924d85584297e96c9d8ddfe9ac67d8de0ca36
nidaj <nida.jawre@gmail.com>
2021-06-12 14:54:26 +0530

    Add : user registration main py file

---
+++ user_registration_main.py
@@ -0,0 +1,8 @@
+from user_registration import UserRegistration
+
+if __name__ == '__main__':
+    user_reg = UserRegistration()
+    first_name_input = input("Enter First Name:")
+    user_reg.first_name_validation(first_name_input)
+    last_name_input = input("Enter Last Name:")
+    user_reg.last_name_validation(last_name_input)
