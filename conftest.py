fdb4d26dadec2753097a897b93ff40d852abb3fc
nidaj <nida.jawre@gmail.com>
2021-06-14 14:18:51 +0530

    Add: pytest file

---
+++ conftest.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+
+@pytest.fixture
+def user_register():
+    from user_registration import UserRegistration
+    return UserRegistration()
---
+++ test_user_registration.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.mark.parametrize("actual,expected",
+                         [("Nida", True), ("NAJ", True), ("nida", False), ("n2", False), ("NIDA", True)])
+def test_first_name(actual, expected, user_register):
+    assert user_register.first_name_validation(actual) == expected
+
+
+@pytest.mark.parametrize("actual,expected",
+                         [("Jawre", True), ("J", False), ("jawre", False), ("Jaw", True), ("JAWRE", True)])
+def test_last_name(actual, expected, user_register):
+    assert user_register.first_name_validation(actual) == expected
